3ec7429700b2f0901ea32bf99ca3de5fe86d7f49
Stefan Helmert <stefan.helmert@t-online.de>
2021-11-18 00:52:57 +0100

    first printable model with valve

--- model/seamless-airmat.py
+++ model/seamless-airmat.py
@@ -6,14 +6,24 @@ import numpy as np
 
 @jit
 def f(x,y,z):
-    prd = 15.
-    r = (((x+0)%33.3 -16.67)**2 + ((y+16.67)%33.3 -16.67)**2)**0.5
+    pl = 100
+    pw = 66.7
+    ph = 8
+    nl = 3
+    nw = 2
+    pre = 9
+    pvl = 50
+    pvr = 10
+    prd = 15
+    r = (((x+pl/nl/2*(nl%2))%(pl/nl)-(pl/nl/2))**2 +
+            ((y+pw/nw/2*(nw%2))%(pw/nw)-(pw/nw/2))**2)**0.5
     rd = r - prd
-    base =12.**2> (x - ((x if x < 100. else 100.) if x > -100. else -100.))**2\
-                 +(y - ((y if y <  50. else  50.) if y >  -50. else  -50.))**2\
-                 +(z - ((z if z <   0. else   0.) if z >   -0. else   -0.))**2
-    hole = (12**2 < rd**2 + z**2) and (prd**2 > r**2)
-    return base and not hole
+    base =pre**2> (x - ((x if x < pl/2 else pl/2) if x > -pl/2 else -pl/2))**2\
+                 +(y - ((y if y < pw/2 else pw/2) if y > -pw/2 else -pw/2))**2\
+                 +(z - ((z if z < ph/2 else ph/2) if z > -ph/2 else -ph/2))**2
+    hole = (pre**2 < rd**2 + (z - ((z if z < ph/2 else ph/2) if z > -ph/2 else -ph/2))**2) and (prd**2 > r**2)
+    valve = (y**2 + z**2) < pvr**2 and x > pl/2 and x < pl/2 + pvl
+    return base and not hole or valve
 
 
 render.renderAndSave(f, 'airmat.stl', 1)
